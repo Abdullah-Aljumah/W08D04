e66ee5b8a383d477594fe63981c544708d1f5e8c
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-01 14:27:14 +0300

    create router update desc

--- routers/controllers/post.js
+++ routers/controllers/post.js
@@ -22,41 +22,55 @@ const newPost = (req, res) => {
 
 // soft delete post
 const softDel = (req, res) => {
-    const { _id } = req.params;
-    try {
-        postModel.findById({ _id: _id }).then((item) => {
-        if (item.isDel == false) {
-            postModel
-            .findByIdAndUpdate(
-              { _id: _id },
-              { $set: { isDel: true } },
-              { new: true }
-            )
-            .then((result) => {
-              res.status(200).json(result);
-            })
-            .catch((err) => {
-              res.status(400).json(err);
-            });
-        } else {
-            postModel
-            .findByIdAndUpdate(
-              { _id: _id },
-              { $set: { isDel: false } },
-              { new: true }
-            )
-            .then((result) => {
-              res.status(200).json(result);
-            })
-            .catch((err) => {
-              res.status(400).json(err);
-            });
-        }
+  const { _id } = req.params;
+  try {
+    postModel.findById({ _id: _id }).then((item) => {
+      if (item.isDel == false) {
+        postModel
+          .findByIdAndUpdate(
+            { _id: _id },
+            { $set: { isDel: true } },
+            { new: true }
+          )
+          .then((result) => {
+            res.status(200).json(result);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      } else {
+        postModel
+          .findByIdAndUpdate(
+            { _id: _id },
+            { $set: { isDel: false } },
+            { new: true }
+          )
+          .then((result) => {
+            res.status(200).json(result);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      }
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+// epdate post
+const updatePost = (req, res) => {
+  const { _id } = req.params;
+  const { desc } = req.body;
+  try {
+    postModel
+      .findOneAndUpdate({ _id: _id }, { $set: { desc: desc } }, { new: true })
+      .then((result) => {
+        res.status(200).json(result);
       });
-    } catch (error) {
-      res.status(400).json(error);
-    }
-  };
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
 
-
-module.exports = {newPost,softDel}
+module.exports = { newPost, softDel, updatePost };
--- routers/routes/post.js
+++ routers/routes/post.js
@@ -3,8 +3,9 @@ const postRouter = express.Router();
 const authentication = require("../authentication");
 const authorization = require("../authorization");
 
-const { newPost, softDel } = require("../controllers/post");
+const { newPost, softDel, updatePost } = require("../controllers/post");
 
+postRouter.put("/updatePost/:_id", updatePost);
 postRouter.post("/newPost/:_id", newPost);
 postRouter.put("/softPost/:_id", softDel);
 
